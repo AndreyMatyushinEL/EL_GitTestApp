c2dfb1371cb975f6c0f2d77d5d8e3e61deae779b
AndreyMatyushinEL <andreym@ulh.ru>
2022-11-28 12:24:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EL_GitTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
